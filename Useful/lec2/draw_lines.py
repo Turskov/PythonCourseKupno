40684a24999ba097f84f86595af35bcbf79489c4
ilya.orlov <orlov.soft.company@gmail.com>
2021-08-13 00:14:45 +0300

    Examples for lec2

--- Useful/lec2/draw_lines.py
+++ Useful/lec2/draw_lines.py
@@ -8,16 +8,16 @@ canvas.pack()
 img = PhotoImage(width=WIDTH, height=HEIGHT)
 canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
-img.put("#ffffff", (13, 2))
-img.put("#ffffff", (13, 3))
-img.put("#ffffff", (13, 4))
-img.put("#ffffff", (13, 5))
-img.put("#ffffff", (13, 6))
-img.put("#ffffff", (13, 7))
-img.put("#ffffff", (13, 8))
-img.put("#ffffff", (13, 9))
-img.put("#ffffff", (13, 10))
-img.put("#ffffff", (13, 11))
+x = 25
+y = 2
+while y < 480:
+    img.put("#ffffff", (x, y))
+    y += 1
 
+x = 26
+y = 2
+while y < 480:
+    img.put("#ffffff", (x, y))
+    y += 1
 
 mainloop()
---
+++ Useful/lec2/functions.py
@@ -0,0 +1,12 @@
+def my_sum(a, b):
+    return a + b
+
+
+x = 10
+y = 20
+s = my_sum(x, y)
+print(s)
+
+my_mul = lambda a, b: a * b
+m = my_mul(10, 20)
+print(m)
