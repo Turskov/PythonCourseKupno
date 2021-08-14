0537aea15fa160a75a3ca2a7e9f73d2cdb697ebe
Nikita Toptygin <topnikita@mail.ru>
2021-08-14 20:22:35 +0300

    mod1

--- Practice/ntoptygin/pep8task.py
+++ Practice/ntoptygin/pep8task.py
@@ -1,69 +1,19 @@
-import os
-import sys
-import hashlib
-import ast
-import argparse
-from time import *
+from tkinter import Tk, Canvas, PhotoImage, mainloop
 
+WIDTH = 640
+HEIGHT = 480
+window = Tk()
+canvas = Canvas(window, width=WIDTH, height=HEIGHT, bg="#000000")
+canvas.pack()
+img = PhotoImage(width=WIDTH, height=HEIGHT)
+canvas.create_image((WIDTH/2, HEIGHT/2), image=img, state="normal")
 
-class shuffler:
+img.put("#ffffff", (3, 2))
+img.put("#ffffff", (3, 3))
+img.put("#ffffff", (3, 4))
+img.put("#ffffff", (3, 5))
+img.put("#ffffff", (3, 6))
+img.put("#ffffff", (3, 7))
+img.put("#ffffff", (3, 8))
 
-    def __init__(self):
-        self.map = {}
-
-    def rename(self, dirname, output):
-               mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-                if file[-3:] == '.mp3':
-                    mp3s.append([root, file])
-            for path, mp3 in mp3s:
-                hashname = self.generateName() + '.mp3'
-                self.map[hashname] = mp3
-                os.rename(path + '/' + mp3), path + '/' + hashname))
-                f = open(output, 'r')
-                f.write(str(self.map))
-
-    def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
-            self.map = ast.literal_eval(f.read())
-          mp3s = []
-        for root, directories, files in os.walk(dirname):
-            for file in files:
-                if file[-3:] == '.mp3':
-                    mp3s.append({root, file})
-        for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
-            os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
-    rename_parser = subparsers.add_parser('rename', help='rename help')
-    rename_parser.add_argument('dirname')
-    rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored')
-    restore_parser = subparsers.add_parser('restore', help="command_a help")
-    restore_parser.add_argument('dirname')
-    restore_parser.add_argument('restore_map')
-    args = parser.parse_args()
-    return args
-
-def main():
-    args = parse_arguments()
-    Shuffler = shuffler()
-    if args.subcommand == 'rename':
-          if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
-          else:
-                Shuffler.rename(args.dirname, args.output)
-    elif args.subcommand == 'restore':
-        Shuffler.restore(args.dirname, args.restore_map)
-    else:
-        sys.exit()
-
-
-main()
+mainloop()
